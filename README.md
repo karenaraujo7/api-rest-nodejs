d9477edadee2a374bb77026125b651488206d0e1
Karen Araujo <karen_araujo7@hotmail.com>
2023-04-25 17:46:02 -0300

    add fastify plugins

---
+++ README.md
@@ -0,0 +1,14 @@
+# Regras funcionais
+
+- [] O usuário deve poder criar uma nova transação
+- [] O usuário deve poder obter um resumo da sua conta
+- [] O usuário deve poder listar todas transações que ja ocorreram
+- [] O usuário deve poder visualizar uma transação única
+
+# Regras de negócio
+
+- [] A transação pode ser do tipo crédito que somará ao valor total, ou débito que subtrairá
+- [] Deve ser possivel identificarmos o usuário entre as requisições
+- [] Ousuário o só pode visualizar trasações o qual ele criou
+
+
---
+++ src/routes/transactions.ts
@@ -0,0 +1,30 @@
+import { FastifyInstance } from 'fastify'
+import { randomUUID } from 'node:crypto'
+import { z } from 'zod'
+import { knex } from '../database'
+
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.post('/', async (req, rep) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(req.body)
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return rep.status(201).send()
+  })
+}
+
+// Modos de fazer comentários
+//todo
+//!
+//?
+//*
